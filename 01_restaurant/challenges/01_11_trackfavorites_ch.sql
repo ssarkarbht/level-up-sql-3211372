cef37d51b78be82e6d05feaeb36cea18704ac17f
Sourav Sarkar <souraviiser@gmail.com>
2023-10-06 19:11:55 +0000

    Restaurant All Solved

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,14 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * from Customers
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater';
+
+SELECT Name FROM Dishes
+WHERE DishID = 14;
+
+UPDATE Customers
+SET FavoriteDish = (SELECT DishID FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad')
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater';
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,12 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT c.FirstName, c.LastName, o.NumOrder
+FROM Customers as c
+JOIN (SELECT CustomerID, COUNT(OrderID) AS NumOrder
+FROM Orders
+GROUP BY CustomerID) AS o 
+ON c.CustomerID = o.CustomerID 
+ORDER BY o.NumOrder DESC
+LIMIT 6;
